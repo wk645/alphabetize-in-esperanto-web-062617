80b0d8dd202928dbdc5ce1237ae137aed83114f7
wk645 <wk645@nyu.edu>
2017-06-25 11:03:35 -0400

    esperanto

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,8 @@
 def alphabetize(arr)
-  # code here
+    esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".chars
+    arr.sort_by do |word|
+        word.chars.collect do |letter|
+            esperanto.index(letter)
+        end
+    end
 end
